6043441d36ceb624b0cf09be0c43d1c63d34d0d7
zhangvalue <zhangvalue@qq.com>
2019-11-27 16:53:22 +0800

    Java读取文件写入文件try-catch
    Java读取文件写入文件try-catch

---
+++ src/main/com.zsf/test/ReadTxt1.java
@@ -0,0 +1,39 @@
+/**
+ * @ Author zhangsf
+ * @CreateTime 2019/11/27 - 13:05
+ */
+package Advanced;
+
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+public class ReadTxt1 {
+    public  static void main(String args[]) {
+        try { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw
+            /* 读入TXT文件 */
+            String pathname = "E:\\python_file\\web_spider\\input.txt"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
+            File filename = new File(pathname); // 要读取以上路径的input。txt文件
+            InputStreamReader reader = new InputStreamReader(
+            new FileInputStream(filename)); // 建立一个输入流对象reader
+            BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
+            String line = "";
+            line = br.readLine();
+            while (line != null) {
+                line = br.readLine(); // 一次读入一行数据
+                System.out.println(line);
+            }
+            /* 写入Txt文件 */
+            File writename = new File("E:\\python_file\\web_spider\\output.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件
+            writename.createNewFile(); // 创建新文件
+            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+            out.write("开始写入文件啦\r\n"); // \r\n即为换行
+            out.flush(); // 把缓存区内容压入文件
+            out.close(); // 最后记得关闭文件
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
